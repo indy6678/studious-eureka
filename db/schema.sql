cf4c5f9758150e4034162cb1ddc47c495ea9bb54
indy6678 <indy6678@gmail.com>
2022-08-29 22:48:16 -0500

    added db, npm init, installed dependencies

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- deletes database if it already exists
+DROP DATABASE tech_blog_db;
+
+-- creates the new database
+CREATE DATABASE tech_blog_db;
+
+-- uses the below database as the default
+USE tech_blog_db;

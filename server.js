01e72183c5af0855800681e1f3ebe545df95fd84
indy6678 <indy6678@gmail.com>
2022-08-29 23:05:26 -0500

    installed nodemon

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const sequelize = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001
 
+sequelize.AsyncQueueError({ force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+});

04eeac835dcf7181ba5699bfe11a7383ea118b9f
indy6678 <indy6678@gmail.com>
2022-08-29 23:20:51 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const sequelize = require('sequelize');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001
 
-sequelize.AsyncQueueError({ force: false}).then(() => {
+sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });

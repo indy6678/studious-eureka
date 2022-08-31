3a1ab53b0f2cf5144f22cb2000e5fdddbde8dd71
indy6678 <indy6678@gmail.com>
2022-08-30 21:59:25 -0500

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+// import parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import database connection from config.js
+const sequelize = require('../config/connection');
+
+// initialize Post model by extending off of sequelize's Model class
+class Post extends Model {}
+
+// set up fields for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        comment: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            user_id: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'user',
+                    key: 'id'
+                }
+            }           
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }    
+);

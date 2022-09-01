a9d88dd4d980b3bf70c8b2e34488c81cfb260bc0
indy6678 <indy6678@gmail.com>
2022-08-31 23:31:53 -0500

    updated connection and homeroute

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {Post, User, Comment} = require('../models')
 
-router.get('/render', (req, res) => {
+router.get('/', (req, res) => {
     res.render('homepage', {
         id: 1,
         title: "Why MVC is so important",
@@ -14,16 +14,16 @@ router.get('/render', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'comments',
-            'created_at',
-            'username'
-        ]
-    })
-})
+// router.get('/', (req, res) => {
+//     Post.findAll({
+//         attributes: [
+//             'id',
+//             'title',
+//             'comments',
+//             'created_at',
+//             'username'
+//         ]
+//     })
+// })
 
 module.exports = router;

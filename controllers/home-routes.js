9783d7b7782ec965874c1937d4b2258ad32b6ceb
indy6678 <indy6678@gmail.com>
2022-09-02 19:07:03 -0500

    updated home-route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const {Post, User, Comment} = require('../models')
 
 router.get('/', (req, res) => {
-    res.send({
+    res.send('homepage',{
         id: 1,
         title: "Why MVC is so important",
         created_at: new Date(),
